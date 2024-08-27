bfcd7b6265e2f10ab5e2bd11f26f8629303c3396
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-27 12:23:59 +0600

    write code for sign up user schema

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -8,3 +8,9 @@ export const usernameValidation = z
     /^[a-zA-Z0-9_]+$/,
     "Only contains alphanumeric characters (letters and numbers)."
   );
+
+  export const singUpSchema = z.object({
+    username: usernameValidation,
+    email:z.string().email({message:"Invalid email address"}),
+    password:z.string().min(6, {message:"password must be at least 6 characters"})
+  })
