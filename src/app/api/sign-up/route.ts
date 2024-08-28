baaa7d9e2b380e2a9e5216ed637b2e0ab3beb846
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-28 09:31:04 +0600

    get the user details form frontend

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -7,7 +7,7 @@ export async function POST(request: Request){
     await dbConnect()
 
     try {
-        
+    const {username, email, password} = await request.json()  
     } catch (error) {
         console.log("Error registering user", error);
         return Response.json(
