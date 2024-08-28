7f13ccc712c7d17cba3f7d1956e11b9f142922c2
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-28 09:29:13 +0600

    write func for register user with resend code and define the error part

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,4 +1,21 @@
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User.model";
 import bcrypt from "bcryptjs"
+
+export async function POST(request: Request){
+    await dbConnect()
+
+    try {
+        
+    } catch (error) {
+        console.log("Error registering user", error);
+        return Response.json(
+            {
+                success:true,
+                message:"Error registering user"
+            }
+        )
+    }
+}
 
