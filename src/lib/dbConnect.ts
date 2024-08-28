1e3355ce03b6695c6b1a01abb92542cc40c19af7
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-28 07:21:20 +0600

    write full func for db connection and export it

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -12,8 +12,14 @@ async function dbConnect(): Promise<void>{
         return
     }
     try {
-        
+        const db = await mongoose.connect(process.env.MONGODB_URI || "",{})
+        console.log("Database",db);
+
+        connection.isConnected = db.connections[0].readyState
     } catch (error) {
-        
+      console.log("Database connection failed", error);
+      process.exit(1)       
     }
 }
+
+export default dbConnect
