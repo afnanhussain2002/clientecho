92c0c1a98cf29bfc5fd0912f3969587014b59e9b
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-29 07:59:49 +0600

    write code for find existing user

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -7,7 +7,18 @@ export async function POST(request: Request){
     await dbConnect()
 
     try {
-    const {username, email, password} = await request.json()  
+    const {username, email, password} = await request.json() 
+    
+    const existingUserVerifiedByUsername = await UserModel.findOne({
+        username,
+        isVerified:true
+    })
+    if (existingUserVerifiedByUsername) {
+        return Response.json({
+            success:false,
+            message:"Username is already taken"
+        }, {status:400})
+    }
     } catch (error) {
         console.log("Error registering user", error);
         return Response.json(
