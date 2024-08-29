0d70bee240c63c84f4361820f33d90177aad104f
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-29 08:11:06 +0600

    write code for verification code

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -20,12 +20,14 @@ export async function POST(request: Request){
         }, {status:400})
     }
    const existingUserByEmail = await UserModel.findOne({email})
+   const verifyCode = Math.floor(100000 + Math.random() * 900000).toString()
     if (existingUserByEmail) {
         true
     }else{
       const hashedPassword = await bcrypt.hash(password,10)
       const expiryDate = new Date()
       expiryDate.setHours(expiryDate.getHours() + 1)
+
     }
     } catch (error) {
         console.log("Error registering user", error);
