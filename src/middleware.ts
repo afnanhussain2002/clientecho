130dee6593699ca166169a9857c090d8c29bb23d
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-29 20:57:12 +0600

    add some path for check tokens

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,10 @@ export async function middleware(request: NextRequest) {
     const url = request.nextUrl
 
     if (token && (
-        url.pathname.startsWith('/sign-in')
+        url.pathname.startsWith('/sign-in') ||
+        url.pathname.startsWith('/sign-up') ||
+        url.pathname.startsWith('/verify') ||
+        url.pathname.startsWith('/')
     )
 ) {
     return NextResponse.redirect(new URL('/dashboard', request.url))
