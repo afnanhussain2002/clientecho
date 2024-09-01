a81a6d21d1c61dce66b76304b19e4a63a57c76af
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-01 12:25:48 +0600

    handle the verify user error part

--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -4,7 +4,19 @@ import UserModel from "@/model/User.model";
 
 export async function POST(request:Request){
     await dbConnect()
-    
+
+    try {
+        
+    } catch (error) {
+        console.log("Error from checking username", error);
+        return Response.json(
+            {
+                success:false,
+                message:"error verifying user"
+            },
+            {status:500}
+        )
+    }
 }
 
 
