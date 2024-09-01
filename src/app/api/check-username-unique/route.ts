c062f5218cc1e9e1a2e459ce7192e05bff651ea4
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-01 11:26:22 +0600

    writing a get method for check the username

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -3,6 +3,19 @@ import UserModel from "@/model/User.model";
 import {z} from "zod"
 import { usernameValidation } from "@/schemas/signUpSchema";
 
+
+
 const UsernameQuerySchema = z.object({
-    
+    username:usernameValidation
 })
+
+export async function GET(request:Request){
+    await dbConnect()
+
+    try {
+        
+    } catch (error) {
+        console.log("Error from checking username", error);
+        
+    }
+}
