1ff131f1752345522afe10d4241a2c863cf9327f
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-01 12:03:23 +0600

    try to find existing verified user

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -20,7 +20,7 @@ export async function GET(request:Request){
     // validate with zod
 
     const result = UsernameQuerySchema.safeParse(queryParam)
-    console.log(result);
+    console.log('main result',result);
 
     if (!result.success) {
         const usernameErrors = result.error.format().username?._errors || []
@@ -33,6 +33,11 @@ export async function GET(request:Request){
             {status:500}
         )
     }
+
+    const {username} = result.data
+
+   const existingVerifiedUser =  await UserModel.findOne({username, isVerified:true})
+
 
 
 
