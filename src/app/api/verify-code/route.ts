85ff5e7cbee73d4286b828597e8c27ce1bf117ec
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-01 12:24:25 +0600

    connect the db and add a post method

--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -1,4 +1,10 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User.model";
-import {z} from "zod"
-import { usernameValidation } from "@/schemas/signUpSchema";
+
+
+export async function POST(request:Request){
+    await dbConnect()
+    
+}
+
+
