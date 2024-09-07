adffdba496fdd86ae5cfe29f4a1b7d38b3566f8a
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-07 12:23:48 +0600

    make try catch and return some response

--- src/app/api/send-message/route.ts
+++ src/app/api/send-message/route.ts
@@ -5,33 +5,50 @@ import { Message } from "@/model/User.model";
 export async function POST(request:Request){
     await dbConnect()
 
-    const {username, content} = await request.json()
-
-    const user = await UserModel.findOne({username})
-
-    if (!user) {
+try {
+        const {username, content} = await request.json()
+    
+        const user = await UserModel.findOne({username})
+    
+        if (!user) {
+            return Response.json(
+                {
+                  success: false,
+                  message: "User not found",
+                },
+                { status: 401 }
+              );
+        }
+        // is user accept message
+    
+        if (!user.isAcceptMessage) {
+            return Response.json(
+                {
+                  success: false,
+                  message: "User not accept message",
+                },
+                { status: 401 }
+              );
+        }
+    
+        const newMessage = {content, createdAt: new Date()}
+    
+        user.messages.push(newMessage as Message)
+        await user.save()
         return Response.json(
             {
-              success: false,
-              message: "User not found",
+              success: true,
+              message: "Message send successfully",
             },
-            { status: 401 }
+            { status: 201 }
           );
-    }
-    // is user accept message
-
-    if (!user.isAcceptMessage) {
-        return Response.json(
-            {
-              success: false,
-              message: "User not accept message",
-            },
-            { status: 401 }
-          );
-    }
-
-    const newMessage = {content, createdAt: new Date()}
-
-    user.messages.push(newMessage as Message)
-    await user.save()
+} catch (error) {
+    return Response.json(
+        {
+          success: false,
+          message: "Unauthorized",
+        },
+        { status: 401 }
+      );
+}
 } 
