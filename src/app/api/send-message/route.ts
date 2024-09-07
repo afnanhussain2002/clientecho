622639b8260a5bf7a1bf59cb3b64bc9e72ecb361
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-07 12:13:30 +0600

    make a post func for send message

--- src/app/api/send-message/route.ts
+++ src/app/api/send-message/route.ts
@@ -1,3 +1,7 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User.model";
 import { Message } from "@/model/User.model";
+
+export async function POST(request:Request){
+    
+} 
