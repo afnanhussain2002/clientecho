0243e7b9e4d0b0ec6993ccbe1cac5916ea4b9189
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-07 10:56:04 +0600

    get the user

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -1,6 +1,12 @@
 import dbConnect from "@/lib/dbConnect";
+import { getServerSession, User } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
 
 
 export async function POST(request: Request) {
     dbConnect();
+   
+   const session = await getServerSession(authOptions)
+   const user: User = session?.user
+
 }
