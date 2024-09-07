1ad3bceaf0f6be0340671c7412d67a2eb01029c2
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-07 11:45:43 +0600

    validate the user

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -7,5 +7,14 @@ export async function POST(request:Request){
 
     const session = await getServerSession(authOptions)
     const user: User = session?.user as User
+    if (!user) {
+        return Response.json(
+          {
+            success: false,
+            message: "Unauthorized",
+          },
+          { status: 401 }
+        );
+    }
 
 }
