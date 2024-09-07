ea37f3816b98aaafe275d86f091870273aea1eb2
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-07 11:22:46 +0600

    get the user on that func

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -59,5 +59,15 @@ export async function POST(request: Request) {
 export async function GET(request: Request) {
   dbConnect();
 
- 
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+  if (!user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Unauthorized",
+      },
+      { status: 401 }
+    );
+  }
 }
