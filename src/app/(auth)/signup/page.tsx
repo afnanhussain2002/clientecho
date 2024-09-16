ab312e4200479cdac803afb9f5b66e32a2ec4a36
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-16 06:18:00 +0600

    add some state

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,11 +1,14 @@
 "use client"
-import React from 'react';
+import React, { useState } from 'react';
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import Link from 'next/link';
 
 const SingUp = () => {
+  const [username, setUsername] = useState('')
+  const [usernameMessage, setUsernameMessage] = useState('')
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false)
   return (
     <div>
       
