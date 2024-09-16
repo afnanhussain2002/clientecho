52604d1ee37fd21ed4ad270b8d89cdf882edc5e7
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-16 07:12:35 +0600

    add form

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,77 +1,147 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import Link from 'next/link';
-import { useDebounceCallback } from 'usehooks-ts'
-import { useToast } from "@/hooks/use-toast"
-import { useRouter } from 'next/navigation';
-import { singUpSchema } from '@/schemas/signUpSchema';
-import axios, { AxiosError } from "axios"
-import { ApiResponse } from '@/types/ApiResponse';
-
+"use client";
+import React, { useEffect, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, useForm } from "react-hook-form";
+import { z } from "zod";
+import Link from "next/link";
+import { useDebounceCallback } from "usehooks-ts";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import { singUpSchema } from "@/schemas/signUpSchema";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 const SingUp = () => {
-  const [username, setUsername] = useState('')
-  const [usernameMessage, setUsernameMessage] = useState('')
-  const [isCheckingUsername, setIsCheckingUsername] = useState(false)
-  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const debounced = useDebounceCallback(setUsername, 500)
-  const { toast } = useToast()
-  const router = useRouter()
+  const debounced = useDebounceCallback(setUsername, 500);
+  const { toast } = useToast();
+  const router = useRouter();
   const form = useForm<z.infer<typeof singUpSchema>>({
-    resolver:zodResolver(singUpSchema),
-    defaultValues:{
-      username:'',
-      email:'',
-      password:''
-    }
-  })
+    resolver: zodResolver(singUpSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
 
-  useEffect(()=>{
+  useEffect(() => {
+    const checkUsernameUnique = async () => {
+      setIsCheckingUsername(true);
+      setUsernameMessage("");
+      try {
+        const response = await axios.get(
+          `/api/check-username-unique?username=${debounced}`
+        );
+        setUsernameMessage(response.data.message);
+      } catch (error) {
+        const axiosError = error as AxiosError<ApiResponse>;
+        setUsernameMessage(
+          axiosError.response?.data.message ?? "Error is checking"
+        );
+      } finally {
+        setIsCheckingUsername(false);
+      }
+    };
+    checkUsernameUnique();
+  }, [debounced]);
 
- const checkUsernameUnique = async() =>{
-  setIsCheckingUsername(true)
-  setUsernameMessage('')
-  try {
-   const response = await axios.get(`/api/check-username-unique?username=${debounced}`)
-    setUsernameMessage(response.data.message)
-  } catch (error) {
-    const axiosError = error as AxiosError<ApiResponse>;
-    setUsernameMessage(axiosError.response?.data.message ?? "Error is checking")
-  }finally{
-    setIsCheckingUsername(false)
-  }
- }
- checkUsernameUnique()
-  },[debounced])
-
-  const onSubmit = async(data: z.infer<typeof singUpSchema>)=> {
-     setIsSubmitting(true)
-     try {
-      const response = await axios.post<ApiResponse>('/api/sign-up',data)
+  const onSubmit = async (data: z.infer<typeof singUpSchema>) => {
+    setIsSubmitting(true);
+    try {
+      const response = await axios.post<ApiResponse>("/api/sign-up", data);
       toast({
-        title:'Success',
-        description:response.data.message
-      })
-      router.replace(`/verify/${username}`)
-      setIsSubmitting(false)
-     } catch (error) {
+        title: "Success",
+        description: response.data.message,
+      });
+      router.replace(`/verify/${username}`);
+      setIsSubmitting(false);
+    } catch (error) {
       const axiosError = error as AxiosError<ApiResponse>;
-      let errorMessage = axiosError.response?.data.message
+      let errorMessage = axiosError.response?.data.message;
       toast({
-        title:"Sing up failed",
-        description:errorMessage,
-        variant:"destructive"
-      })
-      setIsSubmitting(false)
-     }
-  }
+        title: "Sing up failed",
+        description: errorMessage,
+        variant: "destructive",
+      });
+      setIsSubmitting(false);
+    }
+  };
   return (
     <div>
-      
+      <div>
+        <h1>Sign up now</h1>
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="username"
+                    {...field}
+                    onChange={(e) => {
+                      field.onChange(e);
+                      setUsername(e.target.value);
+                    }}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="email"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                  type="password"
+                    placeholder="password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
     </div>
   );
 };
