f41bc9581e388c6afaa56d3917cca8aa73ff7138
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-17 12:18:36 +0600

    npx shadcn@latest add alert-dialog

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -1,30 +1,26 @@
-import React from 'react';
+import React from "react";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
-  
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const MessageCard = () => {
-    return (
-        <Card>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
-      
-    );
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+     
+      </CardContent>
+    
+    </Card>
+  );
 };
 
 export default MessageCard;
