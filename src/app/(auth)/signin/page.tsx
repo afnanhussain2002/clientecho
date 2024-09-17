f7da5c9d7055ecb7c4efacccdda03196e6a6b75e
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-17 09:21:40 +0600

    remove the try catch part

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,14 +1,134 @@
-import { useRouter } from 'next/navigation';
-import React from 'react';
+"use client";
+import React, { useEffect, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import Link from "next/link";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Loader } from "lucide-react";
+import { signInSchema } from "@/schemas/signInSchema";
 
-const SignIn = () => {
-    const router = useRouter()
-    
-    return (
-        <div>
-            
+const SingIn = () => {
+    const [isSubmitting, setIsSubmitting] = useState(false);
+  const { toast } = useToast();
+  const router = useRouter();
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+   
+  };
+  return (
+    <div>
+      <div>
+        <h1>Sign up now</h1>
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="username"
+                    {...field}
+                    onChange={(e) => {
+                      field.onChange(e);
+                      debounced(e.target.value);
+                    }}
+                  />
+                </FormControl>
+              
+                {
+                  isCheckingUsername && <Loader className="animate-spin"/>
+                 
+                }
+                {
+                  username &&  <p className={`${usernameMessage == "Username is unique" ?"text-green-500" : "text-red-500"}`}>{usernameMessage}</p>
+                }
+                
+               
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="email"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                  type="password"
+                    placeholder="password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" disabled={isSubmitting}>
+
+           {
+            isSubmitting ?(
+              <>
+              <Loader/>Please Wait
+              </>
+            ):('Sign up')
+           }
+          </Button>
+        </form>
+      </Form>
+      <div className="text-center mt-4">
+          <p>
+            Already a member?{' '}
+            <Link href="/sign-in" className="text-blue-600 hover:text-blue-800">
+              Sign in
+            </Link>
+          </p>
         </div>
-    );
+    </div>
+  );
 };
 
-export default SignIn;
+export default SingIn;
