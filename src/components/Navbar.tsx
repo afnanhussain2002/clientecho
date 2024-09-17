058472025f5b2b7258fb532ea07893a82b6acba5
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-17 11:45:05 +0600

    add the logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,14 @@ import Link from 'next/link';
 import { useSession, signOut } from 'next-auth/react';
 import { User } from 'next-auth';
 const Navbar = () => {
+    const {data: session} = useSession()
+
+    const user: User = session?.user as User
     return (
         <div>
-            
+            <div>
+                <a href="#">Client Echo</a>
+            </div>
         </div>
     );
 };
