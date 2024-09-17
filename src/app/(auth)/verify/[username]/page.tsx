5b475a8afb7e7407d85f7592155e9a4f4163b67e
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-17 08:22:05 +0600

    handle the error part

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -1,7 +1,9 @@
 
 import { useToast } from '@/hooks/use-toast';
 import { verifySchema } from '@/schemas/verifySchema';
+import { ApiResponse } from '@/types/ApiResponse';
 import { zodResolver } from '@hookform/resolvers/zod';
+import axios, { AxiosError } from 'axios';
 import { useParams, useRouter } from 'next/navigation';
 import React from 'react';
 import { useForm } from 'react-hook-form';
@@ -19,11 +21,24 @@ const VerifyUsername = () => {
       const onSubmit = async(data: z.infer<typeof verifySchema>) =>{
 
 try {
-    
+    const response = await axios.post('/api/verify-code',{
+        username:params.username,
+        code:data.code
+    })
+    toast({
+        title:"Success",
+        description:response.data.message
+    })
+    router.replace('/sign-in')
 } catch (error) {
-    
+    const axiosError = error as AxiosError<ApiResponse>;
+    let errorMessage = axiosError.response?.data.message;
+    toast({
+      title: "Verify Failed",
+      description: errorMessage,
+      variant: "destructive",
+    });
 }
-
       }
     return (
         <div>
