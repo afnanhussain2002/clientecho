b891cc2e3d8bab2a36e62cc7ad4c837d4648c6c7
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-17 11:50:12 +0600

    add some design on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,43 @@
-"use client"
-import React from 'react';
-import Link from 'next/link';
-import { useSession, signOut } from 'next-auth/react';
-import { User } from 'next-auth';
-import { Button } from './ui/button';
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useSession, signOut } from "next-auth/react";
+import { User } from "next-auth";
+import { Button } from "./ui/button";
 const Navbar = () => {
-    const {data: session} = useSession()
+  const { data: session } = useSession();
 
-    const user: User = session?.user as User
-    return (
+  const user: User = session?.user as User;
+  return (
+    <div className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <a href="#" className="text-white text-lg font-semibold">
+          Client Echo
+        </a>
         <div>
-            <div>
-                <a href="#">Client Echo</a>
-            </div>
-            {
-                session ? (
-                    <>
-                    <span>Welcome, {user.username || user.email}</span>
-                    <Button onClick={() => signOut()}>Logout</Button>
-                    </>
-                ) : (
-                    <>
-                    <Link href={'/signin'}>
-                    <Button>Login</Button>
-                    </Link>
-                    </>
-                )
-            }
+          {session ? (
+            <>
+              <span className="text-white mr-4">
+                Welcome, {user.username || user.email}
+              </span>
+              <Button
+                onClick={() => signOut()}
+                className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"
+              >
+                Logout
+              </Button>
+            </>
+          ) : (
+            <Link href={"/signin"}>
+              <Button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+                Login
+              </Button>
+            </Link>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
