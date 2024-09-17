731aa9144e78ba3359c1fa594fc88bb3ada92065
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-17 08:06:58 +0600

    import some code

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -1,14 +1,24 @@
 
 import { useToast } from '@/hooks/use-toast';
+import { verifySchema } from '@/schemas/verifySchema';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useParams, useRouter } from 'next/navigation';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 const VerifyUsername = () => {
     const router = useRouter()
     const params = useParams()
     const {toast} = useToast()
 
-    
+    const form = useForm<z.infer<typeof verifySchema>>({
+        resolver: zodResolver(verifySchema),
+        defaultValues: {
+          email: "",
+          password: "",
+        },
+      });
     return (
         <div>
             
