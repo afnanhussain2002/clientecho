80a3e89fdb6965316a5ad3989c58081e50c72115
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-17 08:43:52 +0600

    complete verification

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -1,11 +1,11 @@
-
-import { Form, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+"use client"
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { useToast } from '@/hooks/use-toast';
 import { verifySchema } from '@/schemas/verifySchema';
 import { ApiResponse } from '@/types/ApiResponse';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button } from '@react-email/components';
 import axios, { AxiosError } from 'axios';
 import { useParams, useRouter } from 'next/navigation';
 import React from 'react';
@@ -52,22 +52,28 @@ try {
             </h1>
             <p className="mb-4">Enter the verification code sent to your email</p>
           </div>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-              <FormField
-                name="code"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Verification Code</FormLabel>
-                    <Input {...field} />
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit">Verify</Button>
-            </form>
-          </Form>
+       <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+        <FormField
+            control={form.control}
+            name="code"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Verification Code</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="code"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+   <Button type='submit'>Submit</Button>
+        </form>
+
+       </Form>
         </div>
       </div>
     );
