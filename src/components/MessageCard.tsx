67fd5d97350bff91663efafd7a0ff0a335b17feb
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-17 12:15:20 +0600

    import some cards

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -1,4 +1,13 @@
 import React from 'react';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  
 
 const MessageCard = () => {
     return (
