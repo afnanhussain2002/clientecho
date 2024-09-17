19a7bbe34267fba366a8b4703e534f5c0ae51487
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-17 12:16:26 +0600

    add some cards

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -11,9 +11,19 @@ import {
 
 const MessageCard = () => {
     return (
-        <div>
-            
-        </div>
+        <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+      
     );
 };
 
