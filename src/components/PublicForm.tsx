de00b983e63a8760cd9440c7194922440a71b13f
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-18 12:24:52 +0600

    build a form

---
+++ src/components/PublicForm.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from './ui/form';
+import { Input } from './ui/input';
+import { Button } from '@react-email/components';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { messageSchema } from '@/schemas/messageSchema';
+import { z } from 'zod';
+
+const PublicForm = () => {
+   
+    const form = useForm<z.infer<typeof messageSchema>>({
+        resolver: zodResolver(messageSchema),
+      });
+
+      const onSubmit = async (data: z.infer<typeof messageSchema>) => {
+
+      }
+
+    return (
+       <>
+         <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Write your message here" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+       </>
+    );
+};
+
+export default PublicForm;
