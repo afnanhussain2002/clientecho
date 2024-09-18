8810298d0cd270863f1db4569d878f49ffd6e95f
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-18 07:45:48 +0600

    add accept message schema

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,7 +1,10 @@
 import { useToast } from '@/hooks/use-toast';
 import { Message } from '@/model/User.model';
+import { acceptMessagesSchema } from '@/schemas/acceptMessageSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useSession } from 'next-auth/react';
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Dashboard = () => {
     const [messages, setMessages] = useState<Message[]>([])
@@ -13,6 +16,10 @@ const Dashboard = () => {
         setMessages(messages.filter((message) => message._id !== messageId))
     }
     const {data:session} = useSession()
+
+    const form = useForm({
+        resolver: zodResolver(acceptMessagesSchema)
+    })
     return (
         <div>
             dashboard
