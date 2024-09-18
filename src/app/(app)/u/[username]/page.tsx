03e64a311c8b69c456cfc821bea1275908e6b24d
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-18 12:06:36 +0600

    fix some error while get the user name

--- src/app/(app)/u/[username]/page.tsx
+++ src/app/(app)/u/[username]/page.tsx
@@ -1,14 +1,23 @@
 'use client'
 import { User } from '@/model/User.model';
+import { Loader2 } from 'lucide-react';
 import { useSession } from 'next-auth/react';
-import React from 'react';
+import React, { useState } from 'react';
 
 const PublicProfile = () => {
-    const {data:session} = useSession()
-   const {username} = session?.user as User
+
+    const {data:session, status} = useSession()
+    
+
+   if (status === 'loading') {
+    return <Loader2/>
+   }
+   if (!session || !session.user) {
+    return <div>User Not Found</div>
+}
     return (
         <div>
-            Public Profile of {username}
+            Public Profile of {session?.user.username}
         </div>
     );
 };
