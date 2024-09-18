95bbc2dda86558c4d49fb76d6d26f2029e80bcab
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-18 08:04:30 +0600

    handle the try catch block

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,7 +1,9 @@
 import { useToast } from '@/hooks/use-toast';
 import { Message } from '@/model/User.model';
 import { acceptMessagesSchema } from '@/schemas/acceptMessageSchema';
+import { ApiResponse } from '@/types/ApiResponse';
 import { zodResolver } from '@hookform/resolvers/zod';
+import axios, { AxiosError } from 'axios';
 import { useSession } from 'next-auth/react';
 import React, { useCallback, useState } from 'react';
 import { useForm } from 'react-hook-form';
@@ -22,15 +24,25 @@ const Dashboard = () => {
     })
     const {register, watch, setValue} = form
 
+    const acceptMessages = watch('acceptMessages')
+
     const fetchAcceptMessage = useCallback(async () =>{
         setIsSwitchLoading(true)
 
         try {
-            
+           const response = await axios.get<ApiResponse>('/api/accept-messages')
+           setValue('acceptMessages',response.data.isAcceptingMessages)
         } catch (error) {
-            
+            const axiosError = error as AxiosError<ApiResponse>;
+            toast({
+                title:"Error",
+                description: axiosError.response?.data.message || "Failed to fetch message settings",
+                variant:"destructive"
+            })
+        }finally{
+            setIsSwitchLoading(false)
         }
-    },[])
+    },[setValue,toast])
 
     return (
         <div>
