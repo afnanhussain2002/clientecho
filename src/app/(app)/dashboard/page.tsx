128e0d940c9094a346fd83ed36b2248cdfba6d5e
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-18 07:41:43 +0600

    handle delete message

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,13 +1,18 @@
 import { useToast } from '@/hooks/use-toast';
 import { Message } from '@/model/User.model';
+import { useSession } from 'next-auth/react';
 import React, { useState } from 'react';
 
 const Dashboard = () => {
     const [messages, setMessages] = useState<Message[]>([])
     const [isLoading, setIsLoading] = useState(false)
     const [isSwitchLoading, setIsSwitchLoading] = useState(false)
-
     const {toast} = useToast()
+
+    const handleDeleteMessage = (messageId:string) =>{
+        setMessages(messages.filter((message) => message._id !== messageId))
+    }
+    const {data:session} = useSession()
     return (
         <div>
             dashboard
