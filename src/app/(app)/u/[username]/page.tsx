42f2b5d03da1eb9c64ecb226202034dfe55c1995
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-19 10:26:37 +0600

    get the username form use params

--- src/app/(app)/u/[username]/page.tsx
+++ src/app/(app)/u/[username]/page.tsx
@@ -3,19 +3,24 @@ import PublicForm from "@/components/PublicForm";
 import { User } from "@/model/User.model";
 import { Loader2 } from "lucide-react";
 import { useSession } from "next-auth/react";
+import { useParams, useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const PublicProfile = () => {
-  const { data: session, status } = useSession();
 
-  if (status === "loading") {
+// const [loading, setLoading] = useState(false)
+const params = useParams()
+const {username} = params
+ 
+
+  /* if (loading) {
     <div className="flex justify-center items-center h-screen bg-gray-100">
     <Loader2 className="w-12 h-12" />
   </div>
-  }
- /*  if (!session || !session.user) {
-    return <div>User Not Found</div>;
   } */
+  if (!username) {
+    return <div className="flex justify-center items-center h-screen bg-gray-100">User Not Found</div>;
+  }
   return (
     <div className="mt-10 flex justify-center">
       <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-md">
@@ -25,7 +30,7 @@ const PublicProfile = () => {
         <div className="bg-gray-100 p-4 rounded-lg">
           <p className="text-center text-lg mb-4">
             Send Anonymous Message to{" "}
-            <span className="font-semibold">@{session?.user.username}</span>
+            <span className="font-semibold">@{username}</span>
           </p>
           <PublicForm />
         </div>
