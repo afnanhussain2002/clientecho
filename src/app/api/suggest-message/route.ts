790cc7cdfba07a4b9c8d0b361601dbe0fac54319
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-19 22:17:38 +0600

    fix route issue

--- src/app/api/suggest-message/route.ts
+++ src/app/api/suggest-message/route.ts
@@ -41,11 +41,16 @@ export async function POST(req:Request) {
    // Send a message to the chat session and log the response
    const result = await chatSession.sendMessage("Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment. And give new questions every time.");
    console.log(result.response.text());
+
+   const candidates = result?.response?.candidates;
+    const message = candidates && candidates.length > 0
+      ? candidates[0].content.parts[0].text?.split('||').map(msg => msg.trim())
+      : [];
  
    return Response.json(
      {
        success: true,
-       message: result?.response?.candidates[0].content.parts[0].text?.split('||').map(msg => msg.trim()),
+       message
      },
      { status: 201 }
    );
