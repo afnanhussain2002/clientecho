2898bdde5745c79b7e43042be1d956f59a953551
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-19 10:41:51 +0600

    remove input and user textarea

--- src/components/PublicForm.tsx
+++ src/components/PublicForm.tsx
@@ -1,50 +1,60 @@
-import React, { useState } from 'react';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from './ui/form';
-import { Input } from './ui/input';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import { messageSchema } from '@/schemas/messageSchema';
-import { z } from 'zod';
-import { Button } from './ui/button';
-import dbConnect from '@/lib/dbConnect';
-import axios, { AxiosError } from 'axios';
-import { ApiResponse } from '@/types/ApiResponse';
-import { toast } from '@/hooks/use-toast';
+import React, { useState } from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { messageSchema } from "@/schemas/messageSchema";
+import { z } from "zod";
+import { Button } from "./ui/button";
+import dbConnect from "@/lib/dbConnect";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+import { toast } from "@/hooks/use-toast";
+import { Textarea } from "./ui/textarea";
 
-const PublicForm = ({username}: {username:string}) => {
-  const [loading, setLoading] = useState(false)
-   
-    const form = useForm<z.infer<typeof messageSchema>>({
-        resolver: zodResolver(messageSchema),
-        defaultValues:{
-          content:""
-        }
+const PublicForm = ({ username }: { username: string }) => {
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof messageSchema>>({
+    resolver: zodResolver(messageSchema),
+    defaultValues: {
+      content: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof messageSchema>) => {
+    setLoading(true);
+    try {
+      const response = await axios.post<ApiResponse>(
+        `/api/send-message/${username}`,
+        data
+      );
+      toast({
+        title: "Success",
+        description: response.data.message,
       });
-
-      const onSubmit = async (data: z.infer<typeof messageSchema>) => {
-     setLoading(true)
-        try {
-         const response =  await axios.post<ApiResponse>(`/api/send-message/${username}`,data)
-         toast({
-          title:'Success',
-          description: response.data.message
-         })
-        } catch (error) {
-          const axiosError = error as AxiosError<ApiResponse>;
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
       let errorMessage = axiosError.response?.data.message;
       toast({
         title: "Message send failed",
         description: errorMessage,
         variant: "destructive",
       });
-        }finally{
-          setLoading(false)
-        }
-           
-      }
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return (
-        <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md">
+  return (
+    <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
           <FormField
@@ -53,12 +63,11 @@ const PublicForm = ({username}: {username:string}) => {
             render={({ field }) => (
               <FormItem>
                 <FormControl>
-                <Input
-  placeholder="Write your message here"
-  {...field}
-  className="w-full h-28 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 align-top"
-/>
-
+                  <Textarea
+                    placeholder="Write your message here"
+                    {...field}
+                    className="w-full h-28 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 align-top"
+                  />
                 </FormControl>
                 <FormMessage className="text-red-500 mt-1" />
               </FormItem>
@@ -73,7 +82,7 @@ const PublicForm = ({username}: {username:string}) => {
         </form>
       </Form>
     </div>
-    );
+  );
 };
 
 export default PublicForm;
