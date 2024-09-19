151ef7ce0d805efe6ebc396c3695b6bdd5cd72ce
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-19 12:32:33 +0600

    fix issues and show the ai message on ui

--- src/components/AiSuggestMessage.tsx
+++ src/components/AiSuggestMessage.tsx
@@ -1,20 +1,40 @@
 import React, { useState } from "react";
 import { Button } from "./ui/button";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+import { toast } from "@/hooks/use-toast";
+import { Loader2 } from "lucide-react";
 
 const AiSuggestMessage = () => {
-    const [messages, setMessages] = useState([])
+  const [messages, setMessages] = useState<string[]>([]);
+  const [loading, setLoading] = useState(false);
 
-    const getAiMessage = async() =>{
-        const response = await axios.get('/api/suggest-message')
-        setMessages(response.data.message)
+  const getAiMessage = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.post("/api/suggest-message");
+      setMessages(response.data.message);
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      let errorMessage = axiosError.response?.data.message;
+      toast({
+        title: "Failed",
+        description: errorMessage,
+        variant: "destructive",
+      });
+    } finally {
+      setLoading(false);
     }
+  };
 
-console.log(messages);
+  console.log(messages);
   return (
     <div className="p-6 bg-gray-100 min-h-screen flex flex-col items-center">
       <div className="mb-6">
-        <Button onClick={getAiMessage} className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+        <Button
+          onClick={getAiMessage}
+          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+        >
           Suggest Messages
         </Button>
         <p className="mt-2 text-gray-700">
@@ -24,17 +44,19 @@ console.log(messages);
 
       <div className="w-full max-w-md bg-white shadow-md rounded-lg p-4">
         <h2 className="text-2xl font-semibold mb-4">Messages</h2>
-        <div className="space-y-2">
-          <p className="p-2 bg-gray-200 rounded hover:bg-gray-300 cursor-pointer">
-            What's your favorite movie?
-          </p>
-          <p className="p-2 bg-gray-200 rounded hover:bg-gray-300 cursor-pointer">
-            Do you have any pets?
-          </p>
-          <p className="p-2 bg-gray-200 rounded hover:bg-gray-300 cursor-pointer">
-            What's your dream job?
-          </p>
-        </div>
+        {loading ? (
+          <h2 className="text-2xl font-semibold mb-4">Generating...</h2>
+        ) : (
+          <div className="space-y-2">
+            {messages?.map((msg) => (
+              <div key={msg}>
+                <p className="p-2 bg-gray-200 rounded hover:bg-gray-300 cursor-pointer">
+                  {msg}
+                </p>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/PublicForm.tsx
+++ src/components/PublicForm.tsx
@@ -78,7 +78,7 @@ const PublicForm = ({ username }: { username: string }) => {
             type="submit"
             className="w-full py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
-            Submit
+            {loading ? 'Sending' : 'Submit'}
           </Button>
         </form>
       </Form>
