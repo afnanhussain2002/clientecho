667485ecc1f62586c5c42bf78fcc94569931cfd5
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-19 11:34:21 +0600

    add a button and p tag also

--- src/components/AiSuggestMessage.tsx
+++ src/components/AiSuggestMessage.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from './ui/button';
 
 const AiSuggestMessage = () => {
     return (
         <div>
-            
+           <div>
+           <Button> Suggest Messages</Button>
+           <p>Click on any message below to select it.</p>
+           </div>
         </div>
     );
 };
